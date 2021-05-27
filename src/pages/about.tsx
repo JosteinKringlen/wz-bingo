846bffa74711ebe68312558de47872bcd8398012
Jostein Kringlen <jostein.kringlen@finn.no>
2021-05-27 17:40:27 +0200

    add animation to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,46 @@
-import { Box, List, Text } from '@dracula/dracula-ui';
+import { Text } from '@dracula/dracula-ui';
+import { motion } from 'framer-motion';
 
 import { alternatives } from '../lib/alternatives';
 
+const container = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            delay: 0.2,
+            staggerChildren: 0.025,
+        },
+    },
+};
+
+const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+};
+
 export default function About(): JSX.Element {
     return (
-        <Box className="pageholder" px="xs">
+        <motion.div
+            className="pageholder drac-px-xs"
+            variants={container}
+            initial="hidden"
+            animate="show"
+        >
             <Text as="p" size="lg" mb="xs">
                 List of words/phrases:
             </Text>
-            <List variant="unordered" color="purple">
+            <motion.ul className="drac-list-unordered drac-list-purple">
                 {alternatives.sort().map((alternative) => (
-                    <li className="drac-text drac-text-white" key={alternative}>
+                    <motion.li
+                        className="drac-text drac-text-white"
+                        key={alternative}
+                        variants={item}
+                    >
                         {alternative}
-                    </li>
+                    </motion.li>
                 ))}
-            </List>
-        </Box>
+            </motion.ul>
+        </motion.div>
     );
 }
